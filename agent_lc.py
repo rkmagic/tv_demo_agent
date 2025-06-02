30fa2e8aac5cbe6dc223625f844aa12f0dac48ba
ninkesh.neema <ninkesh5@gmail.com>
2025-06-02 17:29:18 +0530

    Test ninkesh changes

--- agent_lc.py
+++ agent_lc.py
@@ -14,7 +14,7 @@ from pydantic import BaseModel, Field
 import re
 import streamlit as st
 
-
+#Ninkesh test
 #load_dotenv()
 #openai_api_key=os.getenv('OPENAI_API_KEY')
 #pinecone_api_key=os.getenv('PINECONE_API_KEY')
